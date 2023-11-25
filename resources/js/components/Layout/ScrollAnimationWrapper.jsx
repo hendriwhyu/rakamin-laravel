8d24a763ee8ded5beef770c01898d2d3c103a248
hendriwhyu <hendriwp123@gmail.com>
2023-11-25 23:12:16 +0700

    firstCommit

---
+++ resources/js/components/Layout/ScrollAnimationWrapper.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { motion } from "framer-motion";
+
+export default function ScrollAnimationWrapper({
+    children,
+    className,
+    ...props
+}) {
+    return (
+        <motion.div
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.8 }}
+            className={className}
+            {...props}
+        >
+            {children}
+        </motion.div>
+    );
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <!-- Scripts -->
+    {{-- @routes --}}
+    {{-- @viteReactRefresh --}}
+    @vite('resources/css/app.css')
+    @vite(['resources/js/app.jsx', "resources/js/pages/{$page['component']}.jsx"])
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
